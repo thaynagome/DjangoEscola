1dbaca5b6816bfb06e7e9bcc6cf4095988766801
Thayna Gomes <tluzsantos@hotmail.com>
2019-05-02 15:54:10 -0300

    complemento1

--- base_de_dados/usuarios/models.py
+++ base_de_dados/usuarios/models.py
@@ -7,16 +7,12 @@ class categoria(models.Model):
     def __str__(self):
         return self.nome
 
-class disciplina(models.Model):
-    nome = models.CharField(max_length=100)
-    dt_criacao = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nome
-
 class usuario(models.Model):
     nome = models.CharField(max_length=100)
-    data_acesso = models.DateTimeField()
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    dtNascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento')
+    nrTelCelular = models.CharField(max_length=11, blank=True, null=True, verbose_name='Nº telefone celular')
+    data_acesso = models.DateTimeField(auto_now_add=True)
     categoria = models.ForeignKey(categoria, on_delete=models.CASCADE)
     observacoes = models.TextField(null=True, blank=True)
 
@@ -28,8 +24,11 @@ class usuario(models.Model):
 
 class professor(models.Model):
     nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    dtNascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento')
+    nrTelCelular = models.CharField(max_length=11, blank=True, null=True, verbose_name='Nº telefone celular')
     salario = models.DecimalField(max_digits=7, decimal_places=2)
-    data_acesso = models.DateTimeField()
+    data_acesso = models.DateTimeField(auto_now_add=True)
     categoria = models.ForeignKey(categoria, on_delete=models.CASCADE)
     observacoes = models.TextField(null=True, blank=True)
 
@@ -41,10 +40,13 @@ class professor(models.Model):
 
 class aluno(models.Model):
     nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    dtNascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento')
+    nrTelCelular = models.CharField(max_length=11, blank=True, null=True, verbose_name='Nº telefone celular')
     curso = models.CharField(max_length=100)
     mensalidade = models.DecimalField(max_digits=7, decimal_places=2)
-    data_acesso = models.DateTimeField()
-    disciplina = models.ForeignKey(, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(categoria, on_delete=models.CASCADE)
+    data_acesso = models.DateTimeField(auto_now_add=True)
     observacoes = models.TextField(null=True, blank=True)
 
     class Meta:
